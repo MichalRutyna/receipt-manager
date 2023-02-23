fd8037dfa1fa92a9142b41f8964097426726f661
Michal <mrutyna@interia.pl>
2023-02-23 17:38:13 +0100

    Introduced Purchase class and fixed a few methods

--- main.py
+++ main.py
@@ -3,24 +3,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 import pydantic
-from typing import Union, List
+from typing import List
 
 
 class Item(pydantic.BaseModel):
+    """
+    Represents items known
+
+    Item(name: str,
+        median_price: float,
+
+        category: str)
+    """
     name: str
-    price: float
+    mean_price: float
     category: str
+
+
+class Purchase(pydantic.BaseModel):
+    """
+    Represents individual purchase
+
+    Purchase(item: Item,
+        price: float,
+
+        store: str,
+
+        date: datetime.date)
+    """
+    item: Item
+    price: float
     store: str
     date: datetime.date
-
-    def __repr_args__(self):
-        print("called __repr_args__")
-        return [(None, value) for key, value in self.__dict__.items()]
 
 
 class Database:
     def __init__(self, path):
         self.path = path
+        self.columns = ["price", "store", "date"]
         pd.set_option('display.max_columns', None)
         pd.set_option('display.max_rows', None)
         self.df = pd.read_csv(self.path)
@@ -28,22 +48,16 @@ class Database:
     def print_head(self, rows):
         print(self.df.head(rows))
 
-    def append_item(self, item: List[Item]):
-        thing_to_save = pd.DataFrame([s.__dict__ for s in item])
+    def append_purchase(self, items: List[Purchase]) -> None:
+        """
+        Appends a list of Purchases to the database
+        :param items:
+        :return:
+        """
+        items = [[purchase.item.name, purchase.price, purchase.store, purchase.date] for purchase in items]
+        thing_to_save = pd.DataFrame(items)
         thing_to_save.to_csv(self.path, mode='a', header=False, index=False)
-        # refresh
-        self.df = pd.read_csv(self.path)
-
-    def get_avg_price(self, name):
-        counter = 0
-        price_sum = 0
-        for index, value in self.df['Price'].where(self.df['Name'] == name.capitalize()).dropna().items():
-            counter += 1
-            price_sum += float(value.replace(',', '.'))
-        if counter == 0:
-            return None
-        else:
-            return format(price_sum / counter, '.2f')
+        self.df = pd.read_csv(self.path)  # refresh
 
 
 class Lookup:
@@ -54,52 +68,59 @@ class Lookup:
     def check_category(self, name):
         return self.df['Category'][self.df['Category'].where(self.df['Name'] == name.capitalize()).first_valid_index()]
 
-    def append_item(self, name, category):
-        thing_to_save: pd.DataFrame = pd.DataFrame(name, category)
+    def get_mean_price(self, name):
+        return self.df['Category'][self.df['Category'][2].first_valid_index()]
+
+    # def find_item(self, name):
+    #     name = name.capitalize()
+    #     if (index:= self.df.where(self.df['Name'] == name)) == 0
+    #         return Item(name=name,
+    #                     median_price=self.df["Median_price"].where(self.df['Name'] == name),
+    #                     category=self.df["Category"].where(self.df['Name'] == name))
+    #     else:
+    #         return None
+
+    def test(self, name):
+        idx, names = pd.factorize(self.df['Name'])
+        print(names.get_indexer([name]))
+
+    def append_item(self, item: Item):
+        item = [item.name, item.category, item.mean_price]
+        thing_to_save = pd.DataFrame(item)
         thing_to_save.to_csv(self.path, mode='a', sep=';', header=False, index=False)
 
 
 def main():
-    pass
+    baza_przedmiotow = Lookup('lookup.csv')
+    baza_zakupow = Database('data.csv')
 
-
-if __name__ == '__main__':
-    lookup = Lookup('lookup.csv')
-    baza = Database('data.csv')
-
-    # data.print_head(10)
     print("Witaj w bazie danych!")
     while True:
-        print("-"*50, "\nCo chcesz zrobić?: ")
-        choice = input("(1 - wyswietl dane, 2 - wpisz nowy zakup, 3 - statystyki, q - wyjscie): ")
+        print("-" * 50, "\nCo chcesz zrobić?: ")
+        choice = input("(1 - wyświetl dane, 2 - wpisz nowy zakup, 3 - statystyki, q - wyjście): ")
         match choice:
             case '1':
                 print("Twoje dane:\n")
-                baza.print_head(10)
+                baza_zakupow.print_head(10)
             case '2':
-                print("Wprowadzanie nowej pozycji: \n")
-                baza.append_item(name=input("Podaj nazwę: "),
-                                 price=input("Podaj cenę: "),
-                                 store=input("Podaj sklep: "),
-                                 date=input("Podaj datę (jeśli nie dzisiaj)(DD.MM.RRRR): "))
+                chleb = Item(name="Ser", median_price=19.99, category="Essentials")
+                print("Wprowadzanie nowego zakupu: \n")
+                baza_zakupow.append_purchase([Purchase(item=chleb,
+                                                       price=float(input("Podaj cenę produktu: ")),
+                                                       store=input("Podaj sklep: "),
+                                                       date=datetime.date.today())])
             case '3':
                 nazwa = input("Nazwa produktu: ")
-                print("-"*50)
-                print("Kategoria tego produktu to: ", lookup.check_category(nazwa))
-                print("Średnia cena tego produktu to: ", baza.get_avg_price(nazwa))
+                print("-" * 50)
+                print("Kategoria tego produktu to: ", baza_przedmiotow.check_category(nazwa))
+                print("Średnia cena tego produktu to: ", baza_przedmiotow.check_category(nazwa))
 
             case 'test':
-                x = np.linspace(0, 2 * np.pi, 200)
-                y = np.sin(x)
-
-                fig, ax = plt.subplots()
-                ax.plot(x, y)
-                plt.show()
-
-            case 'x':
-                chleb = Item(name="Ser", price=19.99, category="Essentials", store="Lidl", date=datetime.date.today())
-                szynka = Item(name="Szynka", price=15.99, category="Essentials", store="Lidl", date=datetime.date.today())
-                baza.append_item([chleb, szynka])
+                baza_przedmiotow.test("Poduszki")
 
             case 'q':
                 break
+
+
+if __name__ == '__main__':
+    main()
