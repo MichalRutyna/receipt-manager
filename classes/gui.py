90eb0f11b1f45c26029e6ce4c89096a4d4e4ab19
Michał Rutyna <mrutyna@interia.pl>
2023-03-05 23:18:25 +0100

    Started work on the actual GUI

--- classes/gui.py
+++ classes/gui.py
@@ -8,7 +8,7 @@ class Root(tkinter.Tk):
     def __init__(self):
         super().__init__()
         self.title("Budget manager")
-        self.iconbitmap(default="icon.ico")
+        self.iconbitmap(default="resources/icon.ico")
         self.attributes('-alpha', 0.0)
 
 
@@ -22,8 +22,8 @@ class Window(tkinter.Toplevel):
         self.geometry(f'{int(w/2)}x{int(h/2)}+100+100')
         self.config(background='#121212')
         self.title("Budget manager")
-        self.iconbitmap(default="icon.ico")
-        self.ikona = ImageTk.PhotoImage(Image.open('icon.png').resize((17, 17)))
+        self.iconbitmap(default="resources/icon.ico")
+        self.ikona = ImageTk.PhotoImage(Image.open('resources/icon.png').resize((17, 17)))
         self.update()
 
         # toplevel follows root taskbar events (minimize, restore)
@@ -80,12 +80,15 @@ class Button(tkinter.Button):
     def __init__(self, master, hover_color, **kwargs):
         super().__init__(master, **kwargs)
         if 'font' not in kwargs:
-            self.config(font='Inter 10')
+            self['font'] = 'Inter 10'
+        if 'bg' not in kwargs:
+            self['bg'] = "#222222"
+        if 'borderwidth' not in kwargs:
+            self['borderwidth'] = 2
         self.config(fg='gray',
                     activebackground=brighten(hover_color),
                     activeforeground='white',
-                    relief='sunken',
-                    borderwidth=0)
+                    relief='groove')
         self.org_color = self.cget('bg')
         self.hover_color = hover_color
         self.bind('<Enter>', self.enter)
@@ -109,6 +112,6 @@ def brighten(color):
     B = int(h[4:6], 16)
 
     # rgb -> hex + whitening
-    return rgb2hex(min(int(1.2 * (R + 100)), 255),
-                   min(int(1.2 * (G + 100)), 255),
-                   min(int(1.2 * (B + 100)), 255))
+    return rgb2hex(min(int(1.2 * (R + 20)), 255),
+                   min(int(1.2 * (G + 20)), 255),
+                   min(int(1.2 * (B + 20)), 255))
--- main.py
+++ main.py
@@ -60,11 +60,11 @@ def init_func_frame(master, root):
     title_bar.pack(side='top', fill='x')
 
     title = tkinter.Label(title_bar, image=master.ikona, text=master.wm_title(), fg='gray', bg='black', padx='5px', compound='left')
-    close_button = gui.Button(title_bar, "#FF0000", text='X', font='Inter 13', width=5, bg='black',
+    close_button = gui.Button(title_bar, "#FF0000", text=chr(0x72), font='Marlett', width=5, bg='black', borderwidth=0,
                               command=lambda: root.destroy(), )
-    maximize_button = gui.Button(title_bar, "#FF0000", text=chr(0x32), font='Marlett', width=3, bg='black',
+    maximize_button = gui.Button(title_bar, "#FF0000", text=chr(0x32), font='Marlett', width=3, bg='black', borderwidth=0,
                                  command=lambda: master.malize())
-    inconify_button = gui.Button(title_bar, "#FF0000", text=chr(0x30), font='Marlett', width=3, bg='black',
+    inconify_button = gui.Button(title_bar, "#FF0000", text=chr(0x30), font='Marlett', width=3, bg='black', borderwidth=0,
                                  command=lambda: root.iconify())
     title.pack(side='left')
     close_button.pack(side='right')
@@ -81,10 +81,21 @@ def main():
     root = gui.Root()
     main_window = gui.Window(root)
     init_func_frame(main_window, root)
+    container = tkinter.Label(main_window, bg='#121212')
 
-    content = tkinter.Label(main_window, bg='#121212')
-    content.pack()
+    navigation = tkinter.Label(container, bg='#333333', width=12)
+    navigation_buttons = []
+    navigation_buttons.append(data_list := gui.Button(navigation, "#333333", height=2, text="Wyświetl bazę danych"))
+    navigation_buttons.append(graphs := gui.Button(navigation, "#333333", height=2, text="Pokaż wykresy"))
+    navigation_buttons.append(new_purchases := gui.Button(navigation, "#333333", height=2, text="Dodaj nowy zakup"))
+    for button in navigation_buttons:
+        button.pack(side='top', fill='x')
 
+    content = tkinter.Label(container, bg='#222222')
+
+    navigation.pack(side='left', fill='y')
+    content.pack(fill='both', expand=100)
+    container.pack(fill='both', expand=999)
     root.mainloop()
 
 
